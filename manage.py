f3f7333fd40d0ec33720999fc9875dc2daaa3269
David Garg <davidgarg28@gmail.com>
2022-05-20 22:39:56 +0530

    replaced keys with enviroment variables

--- manage.py
+++ manage.py
@@ -2,10 +2,13 @@
 import os
 import sys
 from django.core.management.commands.runserver import Command as runserver
+from dotenv import load_dotenv
+
+load_dotenv()
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cjapp.settings")
-    runserver.default_port = "8080"
+    runserver.default_port = os.environ['DEFAULT_PORT']
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
