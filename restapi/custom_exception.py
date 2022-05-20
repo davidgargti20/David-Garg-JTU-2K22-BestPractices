f5bc17b426006791f4b7be19e0aa65d1a0b1dae7
David Garg <davidgarg28@gmail.com>
2022-05-20 22:44:04 +0530

    added bad request exception

--- restapi/custom_exception.py
+++ restapi/custom_exception.py
@@ -5,3 +5,8 @@ class UnauthorizedUserException(APIException):
     status_code = 404
     default_detail = "Not Found"
     default_code = "Records unavailable"
+
+class BadRequestException(APIException):
+    status_code = 400
+    default_detail = "Invalid Parameters"
+    default_code = "Bad Request"
